756f1df0f1f0a4126f09e37477cdb329f01f88d0
Jaewon Chung <jaewonchung97@gmail.com>
2023-11-04 13:11:08 +0900

    Dataset, Customer 모델링

--- domain/Customer.py
+++ domain/Customer.py
@@ -1,8 +1,8 @@
 class Customer:
     def __init__(self, x, y, demand, ready_time, due_time, service_time):
-        x = x
-        y = y
-        demand = demand
-        ready_time = ready_time
-        due_time = due_time
-        service_time = service_time
+        self.x = x
+        self.y = y
+        self.demand = demand
+        self.ready_time = ready_time
+        self.due_time = due_time
+        self.service_time = service_time
--- domain/Dataset.py
+++ domain/Dataset.py
@@ -1,6 +1,23 @@
+from Customer import Customer
+from typing import List
+
+
+def get_dist(cus_1: Customer, cus_2: Customer):
+    return ((cus_1.x - cus_2.x) ** 2 + (cus_1.y - cus_2.y) ** 2) ** 0.5
+
+
+def get_metrix(customers: List[Customer]) -> List[List[float]]:
+    result = [[0] * len(customers)] * len(customers)
+    for i in range(len(customers)):
+        for j in range(len(customers)):
+            result[i][j] = get_dist(customers[i], customers[j])
+    return result
+
+
 class Dataset:
-    def __init__(self, x, y, due, customers: list):
-        depot = {x: x, y: y}
-        due_time = due
-        customers = customers
-        distance_matrix = [[]]
+    def __init__(self, num_vehicles, capacity, customers: List[Customer]):
+        self.num_vehicles = num_vehicles
+        self.capacity = capacity
+        self.depot = customers[0]
+        self.customers = customers[1:]
+        self.distance = get_metrix(customers)
