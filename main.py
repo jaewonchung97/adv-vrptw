5e33471acc4c3335e77e6b7e0695fef3643101ac
Jaewon Chung <jaewonchung97@gmail.com>
2023-11-13 03:30:21 +0900

    Run Time Measure

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from typing import List
 
 from domain.Chromosome import Chromosome
@@ -7,10 +8,12 @@ from operators.OperatorConfig import OperatorConfig
 from config import RECOMBINATION_RATE, MIGRATION, MAX_GENERATIONS, POPULATION_SIZE
 import random
 
+
 # from utils.graph_utils import draw_routes
 
 
 def main():
+    start = time.time()
     population_1 = Population()
     population_2 = Population()
     log.info(f"Before--------------------------------------")
@@ -42,6 +45,8 @@ def main():
     log.debug(f"pop2--------------------------------------")
     for idx, chromosome in enumerate(population_2.chromosomes):
         log.debug(f"[{idx}]\t{chromosome}")
+    end = time.time()
+    log.info(f"time: {end - start}")
 
 
 def migration(pop1: Population, pop2: Population) -> None:
